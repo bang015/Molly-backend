e283fb6ad65eda21c3cdfeb04b984dd2caa7cc7e
BDH <dh10162@naver.com>
2024-08-18 21:30:26 +0900

    fix: 미사용 routes 정리

--- src/chat/chat.routes.ts
+++ src/chat/chat.routes.ts
@@ -3,52 +3,13 @@ import { checkJWT } from '../common/middleware/checkJwt';
 import { JwtRequest } from '../auth/auth.interfaces';
 import {
   allUnreadMessageCounts,
-  createChatRoom,
-  createSystemMessage,
-  doesChatRoomExist,
   getChatRoomList,
   getJoinRoomUser,
   getMessageList,
   verifyRoomExists,
 } from './chat.service';
-import { getUser } from '../user/user.service';
 
 const chatRouter = Router();
-chatRouter.post(
-  '/',
-  checkJWT,
-  async (req: JwtRequest, res: Response, next: NextFunction) => {
-    const userId = req.decoded.id;
-    const { chatMembers } = req.body;
-    try {
-      const user = await getUser({ id: userId });
-      if (!user) {
-        return res.status(404).json({ error: '권한이 없습니다.' });
-      }
-      const updatedChatMembers = chatMembers.map((member) => member.id);
-      if (!updatedChatMembers.includes(userId)) {
-        updatedChatMembers.push(userId);
-      }
-      if (chatMembers.length === 1) {
-        const exist = await doesChatRoomExist(userId, chatMembers[0].id);
-        if (exist) {
-          return res.status(200).json(exist.get('roomId'));
-        }
-        const room = await createChatRoom(updatedChatMembers);
-        return res.status(200).json(room);
-      }
-      const room = await createChatRoom(updatedChatMembers, true);
-      const memberNames = chatMembers.map((member) => member.name).join(', ');
-      await createSystemMessage(
-        room,
-        `${user.name}님이 ${memberNames}님을 초대했습니다.`,
-      );
-      return res.status(200).json(room);
-    } catch (e) {
-      return next(e);
-    }
-  },
-);
 
 chatRouter.get(
   '/',
